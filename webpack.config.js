e8a32edc53aa4cc499f8e5d90980888fc65da23f
Marina Khokkonen <mariskasl@mail.ru>
2019-06-11 12:47:16 +0300

    test

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports ={
+    entry: "./src/App.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname , "dist")
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    }
+};
+
