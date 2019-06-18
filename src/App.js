330fded48e37f543c467953becf93a093762a2b8
Marina Hokkonen <mariskasl@mail.ru>
2019-06-19 00:53:54 +0300

    blog

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Blog from './app/component/Blog';
-import WelcomeModal from './app/component/WelcomeModal';
+import {Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
 
-const  POSTS =[{
-            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, quis.',
-            postBody : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, amet aperiam atque dicta dolorem earum est harum, ipsum laudantium maxime officiis quibusdam rerum ullam! Autem culpa deserunt minima mollitia perferendis!'
-        },
-        {
-            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, mollitia?',
-            postBody : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, amet aperiam atque dicta dolorem earum est harum, ipsum laudantium maxime officiis quibusdam rerum ullam! Autem culpa deserunt minima mollitia perferendis!'
-        },
-        {
-            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            postBody : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, amet aperiam atque dicta dolorem earum est harum, ipsum laudantium maxime officiis quibusdam rerum ullam! Autem culpa deserunt minima mollitia perferendis!'
-        },
-        {
-            title: 'Lorem ipsum dolor sit amet.',
-            postBody : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, amet aperiam atque dicta dolorem earum est harum, ipsum laudantium maxime officiis quibusdam rerum ullam! Autem culpa deserunt minima mollitia perferendis!'
-        },
-        {
-            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-            postBody : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, amet aperiam atque dicta dolorem earum est harum, ipsum laudantium maxime officiis quibusdam rerum ullam! Autem culpa deserunt minima mollitia perferendis!'
-        },
-        {
-            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae!',
-            postBody : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, amet aperiam atque dicta dolorem earum est harum, ipsum laudantium maxime officiis quibusdam rerum ullam! Autem culpa deserunt minima mollitia perferendis!'
-        },
-];
+import Layout from './app/layouts/Layout';
+import MainPage from './app/pages/Main';
+import Users from './app/pages/Users';
+import User from './app/pages/User';
+import PageNotFound from './app/pages/PageNotFound';
 
-class App extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            modal : true
-        };
-        this.handleClose = this.handleClose.bind(this);
-    }
+ReactDOM.render(<Router history={browserHistory}>
+    <Route path="/" component={Layout}>
+        <IndexRoute component={MainPage}/>
+        <Route path="users" component={Users}>
+            <Route path=":userId" component={User}/>
+        </Route>
+        <Route path="*" component={PageNotFound}/>
+    </Route>
 
-    handleClose() {
-        this.setState({modal: false});
-    }
-    render() {
-        return <div>
-            {this.state.modal ? <WelcomeModal modal={this.state.modal} onCloseModal={this.handleClose}/> : null }
-            <Blog items={POSTS}  className="Blog" />
-        </div>
-    }
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+</Router>, document.getElementById('root'));
